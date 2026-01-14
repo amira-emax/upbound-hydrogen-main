2de1f226ec78cbaf4c49e1ba33aa7eebdb04319b
amira-emax <amira.farisa@emax-international.com>
2026-01-14 17:46:57 +0800

    try nonce

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -43,7 +43,7 @@ export default async function handleRequest(
       <ServerRouter
         context={reactRouterContext}
         url={request.url}
-        nonce={nonce}
+       
       />
     </NonceProvider>,
     {
--- app/root.tsx
+++ app/root.tsx
@@ -186,7 +186,7 @@ export function Layout({ children }: { children?: React.ReactNode }) {
     <html lang="en">
       <head>
         <script
-          nonce={nonce}
+          nonce={useNonce()}
           dangerouslySetInnerHTML={{
             __html: `
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':

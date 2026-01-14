e4b5c6938af3fde147655063ecf1b3d69ca939c8
amira-emax <amira.farisa@emax-international.com>
2026-01-14 16:52:13 +0800

    remove debug

--- app/root.tsx
+++ app/root.tsx
@@ -114,8 +114,6 @@ async function loadCriticalData({ context }: LoaderFunctionArgs) {
     // Add other queries here, so that they are loaded in parallel
   ]);
 
-  console.log('sini', header);
-
 
   return { header };
 }

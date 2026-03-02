30209b78090511b20ba02c45a3a82942cbe3170f
amira-emax <amira.farisa@emax-international.com>
2026-03-02 16:22:24 +0800

    update discount label

--- app/components/ProductPrice.tsx
+++ app/components/ProductPrice.tsx
@@ -48,16 +48,21 @@ export function ProductPrice({
   const activePrice = isSubscriptionActive ? subPrice : price;
   const activeCompareAtPrice = isSubscriptionActive ? price : compareAtPrice;
 
+  let discountPercent = 0;
+  if (activePrice && activeCompareAtPrice) {
+    const original = parseFloat(activeCompareAtPrice.amount);
+    const current = parseFloat(activePrice.amount);
+
+    if (original > current) {
+      discountPercent = Math.round(((original - current) / original) * 100);
+    }
+  }
+
   return (
     <div className={cn('product-price', className)}>
       {activeCompareAtPrice ? (
         <div className="product-price-on-sale flex items-center gap-2">
-          {activePrice ? (
-            <div className={cn('text-orange typo-paragraph', priceClassName)}>
-              RM
-              <Money withoutCurrency data={activePrice} className="inline" />
-            </div>
-          ) : null}
+
           <s>
             <span className={cn('text-gray-400', compareAtPriceClassName)}>
               RM
@@ -68,6 +73,20 @@ export function ProductPrice({
               />
             </span>
           </s>
+          {activePrice ? (
+            <div className={cn('text-secondary-green typo-header flex items-center gap-3')}>
+              <div>
+                RM <Money withoutCurrency data={activePrice} className="inline" />
+              </div>
+
+              {/* 👇 Conditionally render the dynamic badge */}
+              {discountPercent > 0 && (
+                <span className="bg-red-500 text-white px-3 py-0.5 rounded-full text-[15px] uppercase">
+                  Save {discountPercent}%
+                </span>
+              )}
+            </div>
+          ) : null}
         </div>
       ) : activePrice ? (
         <div className={cn('typo-paragraph', priceClassName)}>
